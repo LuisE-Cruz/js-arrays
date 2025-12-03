e5730e116ce565222bb66ac8cdb1b1e911bb9f1b
LuisE Cruz <Luis.cruzlc01@gmail.com>
2025-12-03 18:00:11 +0100

    Consegna quesito 5 esercizio bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -11,8 +11,15 @@ const teachers = [
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
 
+// const reversedTeachers = []
+
+// for(let i = 8; i > teachers.length; i--){
+//   reversedTeachers.push(teachers[i]);
+//   console.log(reversedTeachers);
+// }
+
 const reversedTeachers = teachers.toReversed();
-// console.log(reversedTeachers);
+console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
@@ -50,4 +57,5 @@ const isFabioPresent = fabioFound;
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 
-const teachersString = null;
+const teachersString = teachers.join();
+// console.log(teachersString);
